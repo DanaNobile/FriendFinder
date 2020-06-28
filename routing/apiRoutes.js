8ed9abd780693f5459aaca122b4d46ca06cb574a
Dana Nobile <danagnobile@gmail.com>
2020-06-28 16:45:12 -0400

    Created home html page

--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -18,13 +18,33 @@ module.exports = function (app) {
         // waitListData.length = 0;
         var newPerson = req.body;
 
+        // var newPersonTotalScore= 10;
+        // var lowestDifference = 2;
+        // var lowestMatch = ""
+
+
+
         for (let i = 0; i < friendsData.length; i++) {
             const element = friendsData[i];
             const elementTotalScore = 8;
             console.log(element);
         }
 
+        // if (friendsData.length < 5) {
+        //     tableData.push(req.body);
+        //     res.json(true);
+        //   }
+        //   else {
+        //     waitListData.push(req.body);
+        //     res.json(false);
+        //   }
+
         res.send("Hello World");
         res.json({ ok: true });
     });
+
+    // app.post("/api/clear", function (req, res) {
+    //     res.json({ ok: true });
+    // })
+
 };
